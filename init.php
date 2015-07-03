a07c6175800bdf71b5f26f9efaa99db164da2a72
Gagan Deep Singh <me@gagan.pro>
2015-07-03 13:30:26 +0530

    Added fixes for the remaining orientiations and made use of WP Image Editor

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @package Image Rotation Repair
+ * @package Fix Image Rotation
  * @version 1.0
  */
 /*
@@ -24,80 +24,75 @@ if (!class_exists('FixImageRotation')) {
         }
 
         public function filter_wp_handle_upload($file) {
-            $this->fixImageOrientation($file['file'], $file['type']);
+            $this->fixImageOrientation($file['file']);
             return $file;
         }
 
         public function filter_wp_handle_upload_prefilter($file) {
-            $suffix = substr($file['name'], strrpos($file['name'], '.') + 1); // I know there's a better way to get a file type / mime_type.
-            switch (strtolower($suffix)) {
-                case 'jpg':
-                case 'jpeg':
-                    $type = 'image/jpeg';
-                    break;
-                case 'png':
-                    $type = 'image/png';
-                    break;
-                case 'gif':
-                    $type = 'image/gif';
-                    break;
-            }
-            if (isset($type)) {
-                $this->fixImageOrientation($file['tmp_name'], $type);
+            $suffix = substr($file['name'], strrpos($file['name'], '.') + 1);
+            if (in_array($suffix, array('jpg', 'jpeg', 'png', 'gif'))) {
+                $this->fixImageOrientation($file['tmp_name']);
             }
             return $file;
         }
 
-        public function fixImageOrientation($file, $type) {
+        public function fixImageOrientation($file) {
             if (is_callable('exif_read_data') && !isset($this->oreintation_fixed[$file])) {
-                $exif = @exif_read_data($file);
+                $exif = exif_read_data($file);
                 if (isset($exif) && isset($exif['Orientation']) && $exif['Orientation'] > 1) {
                     include_once( ABSPATH . 'wp-admin/includes/image-edit.php' );
-                    error_log(var_export($exif, true));
+                    $rotator = false;
+                    $flipper = false;
                     switch ($exif['Orientation']) {
+                        case 1:
+                            //We don't want to fix an already correct image :)
+                            $this->orientation_fixed[$file] = true;
+                            return;
+                        case 2:
+                            $flipper = array(false, true);
+                            break;
                         case 3:
                             $orientation = -180;
+                            $rotator = true;
+                            break;
+                        case 4:
+                            $flipper = array(true, false);
+                            break;
+                        case 5:
+                            $orientation = -90;
+                            $rotator = true;
+                            $flipper = array(false, true);
                             break;
                         case 6:
                             $orientation = -90;
+                            $rotator = true;
+                            break;
+                        case 7:
+                            $orientation = -270;
+                            $rotator = true;
+                            $flipper = array(false, true);
                             break;
                         case 8:
                         case 9:
                             $orientation = -270;
+                            $rotator = true;
                             break;
                         default:
                             $orientation = 0;
+                            $rotator = true;
                             break;
                     }
-                    switch ($type) {
-                        case 'image/jpeg':
-                            $image = imagecreatefromjpeg($file);
-                            break;
-                        case 'image/png':
-                            $image = imagecreatefrompng($file);
-                            break;
-                        case 'image/gif':
-                            $image = imagecreatefromgif($file);
-                            break;
-                        default:
-                            $image = false;
-                            break;
+
+                    $editor = wp_get_image_editor($file);
+                    if (!is_wp_error($editor)) {
+                        if ($rotator === true) {
+                            $editor->rotate($orientation);
+                        }
+                        if ($flipper !== false) {
+                            $editor->flip($flipper[0], $flipper[1]);
+                        }
+                        $editor->save($file);
                     }
-                    if ($image) {
-                        $image = WP_Image_Editor::rotate()
-                        $image = _rotate_image_resource($image, $orientation);
-                        switch ($type) {
-                            case 'image/jpeg':
-                                imagejpeg($image, $file, apply_filters('jpeg_quality', 90, 'edit_image'));
-                                break;
-                            case 'image/png':
-                                imagepng($image, $file);
-                                break;
-                            case 'image/gif':
-                                imagegif($image, $file);
-                                break;
-                        } // end switch
-                    } // end if $image
                 } // end if $exif
             } // end is_callable('exif_read_data')
             $this->orientation_fixed[$file] = true;
@@ -107,4 +102,3 @@ if (!class_exists('FixImageRotation')) {
 
     new FixImageRotation();
 }
-?>
