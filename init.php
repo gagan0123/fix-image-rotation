02d552eb1086c07c6c0cbd795d7dc1c04487fbb6
Gagan Deep Singh <me@gagan.pro>
2017-09-10 12:02:22 +0530

    PHPCS Corrections in documentation

--- init.php
+++ init.php
@@ -1,20 +1,21 @@
 <?php
-
-/*
-  Plugin Name: Fix Image Rotation
-  Plugin URI: https://wordpress.org/plugins/fix-image-rotation/
-  Description: Fix Image Rotation plugin fixes image orientation based on EXIF data.  This is primarily a patch for mis-oriented images delivered from iPhones.  Functionally it filters all uploads and if EXIF->Orientation is set to a number greater than 1, then the image is resaved with a new orientation before the image is processed by WordPress.
-  Author: Gagan Deep Singh
-  Version: 2.0
-  Author URI: http://gagan.pro
-  License: GPLv2
-  License URI: https://www.gnu.org/licenses/gpl-2.0.html
-  Text Domain: fix-image-rotation
-  Domain Path: /languages
+/**
+ * Plugin Name: Fix Image Rotation
+ * Plugin URI: https://wordpress.org/plugins/fix-image-rotation/
+ * Description: Fix Image Rotation plugin fixes image orientation based on EXIF data.  This is primarily a patch for mis-oriented images delivered from iPhones.  Functionally it filters all uploads and if EXIF->Orientation is set to a number greater than 1, then the image is resaved with a new orientation before the image is processed by WordPress.
+ * Author: Gagan Deep Singh
+ * Version: 2.0
+ * Author URI: http://gagan.pro
+ * License: GPLv2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: fix-image-rotation
+ * Domain Path: /languages
+ *
+ * @package Fix_Image_Rotation
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit; // Exit if accessed directly.
 }
 
 if ( ! defined( 'GS_FIR_PATH' ) ) {
